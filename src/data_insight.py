546a392ac191e17e8d67d91cf19f5eba88e07268
Shikhar Gupta <sgupt330@asu.edu>
2023-05-11 14:29:46 -0700

    Normalizing json file

--- src/data_insight.py
+++ src/data_insight.py
@@ -1,3 +1,5 @@
+import json
+
 import numpy as np
 from random import shuffle
 import os
@@ -10,10 +12,11 @@ class DataInsight:
     def __init__(self):
 
         self.gen_data = gd.DataGenerate()
-        self.modified_training_data_file_path = "data/training_data_v2.npy"
+        self.modified_training_data_file_path = "labels_v2.json"
 
     def individualChoices(self, training_data_file_path):
-        train_data = np.load(training_data_file_path)
+        with open(training_data_file_path, "r+") as fp:
+            train_data = json.load(fp)
 
         shuffle(train_data)
 
@@ -21,43 +24,40 @@ class DataInsight:
         down = []
         nothing = []
 
-        for data in train_data:
-            img = data[0]
-            move = data[1]
+        for img, move in train_data:
             if move == self.gen_data.key_to_int["up"]:
-                up.append([img, move])
+                up.append({img: move})
             elif move == self.gen_data.key_to_int["down"]:
-                down.append([img, move])
+                down.append({img: move})
             else:
-                nothing.append([img, move])
+                nothing.append({img: move})
         return up, down, nothing
 
     def main(self):
         final_data = []
-        # dir is your directory path
-        file_names = os.listdir("data/")
-        number_files = len(file_names)
-        file_names.sort()
-        print(file_names)
-        for i in range(number_files):
-            print("file" + str(i + 1))
+        # # dir is your directory path
+        # file_names = os.listdir("data/")
+        # number_files = len(file_names)
+        # file_names.sort()
+        # print(file_names)
+        # for i in range(number_files):
+        up, down, nothing = self.individualChoices(self.gen_data.training_data_file_path)
 
-            up, down, nothing = self.individualChoices(self.gen_data.training_data_file_path + str(i + 1) + ".npy")
+        total_data_len = len(up) + len(down) + len(nothing)
 
-            total_data_len = len(up) + len(down) + len(nothing)
+        up_factor = len(up) / total_data_len
+        down_factor = len(down) / total_data_len
+        nothing_factor = len(nothing) / total_data_len
 
-            up_factor = len(up) / total_data_len
-            down_factor = len(down) / total_data_len
-            nothing_factor = len(nothing) / total_data_len
+        up = up[:int(up_factor * len(up))]
+        down = down[:int(down_factor * len(down))]
+        nothing = nothing[:int(nothing_factor * len(nothing))]
 
-            up = up[:int(up_factor * len(up))]
-            down = down[:int(down_factor * len(down))]
-            nothing = nothing[:int(nothing_factor * len(nothing))]
+        final_data += up + down + nothing
+        print(final_data)
+        shuffle(final_data)
 
-            final_data += up + down + nothing
-            shuffle(final_data)
-
-        np.save(self.modified_training_data_file_path, final_data)
+        # np.save(self.modified_training_data_file_path, final_data)
 
 # to run this file independently
 # z=DataInsight()
